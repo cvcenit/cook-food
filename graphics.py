201d5cc579afb3d244cf3ce22bb2493812844271
Chicco <cvcenit@up.edu.ph>
2026-05-18 00:30:24 +0800

    main menu almost finished, will continue tom

--- graphics.py
+++ graphics.py
@@ -1,6 +1,11 @@
 from abc import ABC, abstractmethod
 import pyxel
 
+FPS = 30
+SCREEN_WIDTH = 720
+SCREEN_HEIGHT = 720
+HEADER_FONT_SIZE = 48
+HEADER_FONT = pyxel.Font("./eater.ttf", font_size=HEADER_FONT_SIZE)
 
 class Button(ABC):
 	@abstractmethod
@@ -10,22 +15,119 @@ class Button(ABC):
 	@abstractmethod
 	def is_hovered(self) -> bool:
 		...
-	
 
 class TextButton(Button):
-	def __init__(self, view):
-		img = pyxel.Image.text(0, 0, "HELLO", 0, font=None)
+	def __init__(self, x, y, text, color):
+		self._x, self._y, self._text, self._color = x, y, text, color
+		self._text_width = HEADER_FONT.text_width(self._text)
 
-	def is_clicked(self):
-		...
+	def is_clicked(self) -> bool:
+		if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
+			return self.is_hovered()
+		return False
+
+	def is_hovered(self):
+		return self._x <= pyxel.mouse_x <= self._x + self._text_width and \
+		self._y <= pyxel.mouse_y <= self._y + HEADER_FONT_SIZE
+
+	def draw_button(self):
+		text(self._x, self._y, self._text, (8 if self.is_hovered() else self._color), font=HEADER_FONT)
 
 class SpriteButton(Button):
 	pass
 
-class Screen:
-	# Will have its own model, view, controller
-	# its only methods are update and draw, which is also the update of the controller, and the draw of the view
-	pass
+class Screen(ABC):
+	@abstractmethod
+	def update(self):
+		...	
+
+	@abstractmethod
+	def draw(self):
+		...
+
+class MainMenuModel:
+	def __init__(self):
+		self._buttons = [
+		TextButton(50, 48, "Play", 1),
+		TextButton(50, 96, "Settings", 1)
+		]
+		self._states = ["game", "settings"]
+		self._current_tick = 1
+		self._is_current_screen = True
+
+	@property
+	def is_current_screen(self):
+		return self._is_current_screen
+
+	@property
+	def buttons(self):
+		return self._buttons
+	
+	@property
+	def next_state(self):
+		if self.is_current_screen:
+			raise ValueError
+		return self._next_state
+	
+
+	def update(self, clicked_idx):
+		if self._is_current_screen:
+			self._current_tick += 1
+			if clicked_idx is not None:
+				self.change_screen(self._states[clicked_idx])
+
+	def change_screen(self, state):
+		self._is_current_screen = False
+		self._current_tick = 1
+		return state
+
+class MainMenuView:
+	def __init__(self, width, height):
+		self._width, self._height = width, height
+        self._bg_color: int = 6
+        self._left_margin: float = self._width / 10
+        self._top_margin: float = self._height / 4
+
+    def draw_buttons(self, buttons) -> None:
+        for button in buttons:
+            button.draw_button()
+
+    def reset_screen(self) -> None:
+        pyxel.cls(self._bg_color)
+
+class MainMenuController:
+	def __init__(self, model, view, app):
+		self._model = model
+		self._view = view
+		self._app = app
+
+	def update(self):
+		if not self.model.is_current_screen:
+			self.switch_screen(self._model.next_state)
+
+		clicked_idx = self._view.get_clicked_button(self._model.buttons)
+        self._model.update(clicked_idx)
+
+    def draw(self):
+    	self._view.reset_screen()
+    	self._view.draw_buttons(self._model.buttons)
+
+    def switch_screen(self):
+    	self._app.switch_screen
+
+class MainMenu(Screen):
+	def __init__(self, app):
+		self._app = app
+		self._model = MainMenuModel()
+		self._view = MainMenuView(SCREEN_WIDTH, SCREEN_HEIGHT)
+		self._controller = MainMenuController(self._model, self._view, self._app)
+
+	def update(self):
+		self._controller.update()
+
+	def draw(self):
+		self._controller.draw()
+
 
 class GridLayout:
 	# ang demanding
