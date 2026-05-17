df882929024a8c886a85278ed4fe72f3d85a8122
Chicco <cvcenit@up.edu.ph>
2026-05-17 23:17:36 +0800

    merged?

--- graphics.py
+++ graphics.py
@@ -1,17 +1,30 @@
-from collections.abc import ABC
+from abc import ABC, abstractmethod
+import pyxel
 
 
-class Button:
-	pass
+class Button(ABC):
+	@abstractmethod
+	def is_clicked(self) -> bool:
+		...
+
+	@abstractmethod
+	def is_hovered(self) -> bool:
+		...
+	
 
 class TextButton(Button):
-	pass
+	def __init__(self, view):
+		img = pyxel.Image.text(0, 0, "HELLO", 0, font=None)
+
+	def is_clicked(self):
+		...
 
 class SpriteButton(Button):
 	pass
 
 class Screen:
-	# Menu or screen? i think screen mas fitting
+	# Will have its own model, view, controller
+	# its only methods are update and draw, which is also the update of the controller, and the draw of the view
 	pass
 
 class GridLayout:
