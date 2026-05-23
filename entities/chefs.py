5c0a2d16d1dfe4255221c04ef5d5838de97cd833
Chicco <cvcenit@up.edu.ph>
2026-05-23 12:39:25 +0800

    made chef a 'tower'

--- entities/chefs.py
+++
@@ -1,5 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Chef(ABC):
-    def __init__(self):
-        pass
--- entities/towers.py
+++ entities/towers.py
@@ -1,5 +1,12 @@
 from abc import ABC, abstractmethod
 
-class Tower(ABC):
+class TowerInfo(ABC):
+    ...
+
+class Tower(TowerInfo):
+    def __init__(self):
+        pass
+
+class Chef(Tower):
     def __init__(self):
         pass
