e395333784197cdde7331f6b403dee27987597af
Janella Mairabelle Campo <133028903+nella-cmp@users.noreply.github.com>
2026-06-03 02:26:00 +0800

    naoverwrite q hehe binalik q lg

--- graphics.py
+++ graphics.py
@@ -58,6 +58,10 @@ class TextButton(Button):
         self._x = x
         self._y = y
 
+    def change_text(self, t):
+        self._text = t
+        self._width = self._font.text_width(self._text)
+    
     def change_color(self, col):
         self._color = col
 
@@ -119,7 +123,7 @@ class SpriteButton(Button):
             image_bank,
             position_x, position_y,
             width, height,
-            11,
+            13,
             scale=self._scale
             )
     @property
@@ -129,6 +133,48 @@ class SpriteButton(Button):
     def change_position(self, x, y):
         self._x = x
         self._y = y
+
+class TextInput:
+    def __init__(self, x, y, color, size=24):
+        self._x, self._y, self._color, self._size = x, y, color, size
+        self._font = pyxel.Font("./resources/eater.ttf", font_size=self._size)
+        self._text = "Enter your name"
+        self._width = self._font.text_width(self._text)
+
+        self._is_active = True
+
+    @property
+    def width(self):
+        return self._width
+
+    def listen(self):
+        for i in range(97, 123):
+            if pyxel.btnp(i):
+                if self._text == "Enter your name":
+                    self._text = ""
+                self.change_text(self._text + chr(i))
+        if pyxel.btnp(pyxel.KEY_BACKSPACE, repeat=3):
+            if self._text == "Enter your name":
+                self._text = ""
+            self.change_text(self._text[:-1])
+
+    @property
+    def current_position(self):
+        return self._x, self._y    
+
+    def change_position(self, x, y):
+        self._x, self._y = x, y
+
+    def toggle_active(self):
+        self._is_active = not self._is_active
+
+    def draw_text(self):
+        if self._is_active:
+            pyxel.text(self._x, self._y, self._text, self._color, font=self._font)
+
+    def change_text(self, t):
+        self._text = t
+        self._width = self._font.text_width(self._text)
 
 class TextGraphic:
     def __init__(self, x, y, text, color, size=24):
@@ -159,6 +205,10 @@ class TextGraphic:
     def draw_text(self):
         if self._is_active:
             pyxel.text(self._x, self._y, self._text, self._color, font=self._font)
+
+    def change_text(self, t):
+        self._text = t
+        self._width = self._font.text_width(self._text)
 
 class PopupScreen:
     def __init__(self, x, y, width, height, buttons, texts, bg):
