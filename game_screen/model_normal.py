544cc0dd57d28fb736d8ef878da8e66e96c523a5
Janella Campo <jlcampo1@up.edu.ph>
2026-06-05 09:21:12 -0400

    bgm added

--- game_screen/model_normal.py
+++ game_screen/model_normal.py
@@ -477,6 +477,8 @@ class GameModel:
         for screen in self.popup_screens:
             if screen.is_active:
                 screen.toggle_active()
+            else:
+                pyxel.play(0, 0, loop=True)
 
     def reset(self):
         self._current_tick = 1
@@ -489,6 +491,7 @@ class GameModel:
         self._register_message = None
 
     def update_game_over(self):
+        pyxel.stop(0)
         if self._game_logic.is_game_won:
             win_popup = self.popup_screens[4]
             if not win_popup.is_active:
@@ -513,7 +516,7 @@ class GameModel:
             elif clicked_idx == 2:
                 self.reset()
             else:
-                ...
+                pyxel.stop(0)
 
     def update_from_direction_menu(self, d):
         if d is not None:
